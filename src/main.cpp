c3800ac2f85bdef32b53d9e5a078c909633a098a
luna 3 <93050744+astranema@users.noreply.github.com>
2026-06-11 14:05:21 -0400

    Added second circle
    This is on a new branch because it isn't serious and isn't related to the main project. should be deleted eventually

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+	sf::RenderWindow window( sf::VideoMode( { 400, 400 } ), "SFML works!" );
 	sf::CircleShape shape( 100.f );
 	shape.setFillColor( sf::Color::Magenta );
+	shape.setPosition( sf::Vector2f( { 100, 100 }));
 
 	while ( window.isOpen() )
 	{
+		sf::CircleShape shape2( 100.f );
+		shape2.setFillColor( sf::Color::Green );
+		shape2.setPosition( sf::Vector2f( { 50, 100 }));
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
@@ -16,6 +20,7 @@ int main()
 
 		window.clear();
 		window.draw( shape );
+		window.draw( shape2 );
 		window.display();
 	}
 }
