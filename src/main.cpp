4fda2a7470ef60e1a887819c47be509380875fbe
luna 3 <93050744+astranema@users.noreply.github.com>
2026-06-14 16:41:45 -0400

    added direction enum, added keyboard input management
    additionally, moved the order of classes to fix a bug because of them not being able to access classes above them in code. could likely be fixed by declaring the classes before where they're actually coded, that could be easier in the future

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,5 @@
 #include <SFML/Graphics.hpp>
 
-class DoublyLinkedList {
-private:
-	Node* head;
-	Node* tail;
-public:
-	Node* get_head() {
-		return head;
-	}
-	void set_head(Node* node) {
-		this->head = node;
-	}
-	Node* get_tail() {
-		return tail;
-	}
-	void set_tail(Node* node) {
-		this->tail = node;
-	}
-};
-// node of doubly linked list
-class Node {
-private:
-	Coords coords;
-	Node* next;
-	Node* prev;
-public:
-	Node(int x, int y) {
-		this->coords = Coords(x, y);
-		next = nullptr;
-	}
-	Node* get_next() {
-		return next;
-	}
-	void set_next(Node* node) {
-		this->next = node;
-	}
-	Node* get_prev() {
-		return prev;
-	}
-	void set_next(Node* prev) {
-		this->prev = prev;
-	}
-};
 // coordinates with two ints. 
 class Coords {
 private:
@@ -69,11 +27,75 @@ public:
 		return this->y;
 	}
 };
+// node of doubly linked list
+class Node {
+private:
+	Coords coords;
+	Node* next;
+	Node* prev;
+public:
+	Node(int x, int y) {
+		this->coords = Coords(x, y);
+		next = nullptr;
+		prev = nullptr;
+	}
+	Node* get_next() {
+		return next;
+	}
+	void set_next(Node* node) {
+		this->next = node;
+	}
+	Node* get_prev() {
+		return prev;
+	}
+	void set_prev(Node* node) {
+		this->prev = node;
+	}
+};
+class DoublyLinkedList {
+private:
+	Node* head;
+	Node* tail;
+public:
+	Node* get_head() {
+		return head;
+	}
+	void set_head(Node* node) {
+		this->head = node;
+	}
+	Node* get_tail() {
+		return tail;
+	}
+	void set_tail(Node* node) {
+		this->tail = node;
+	}
+	void push_head(Node* node) {
+		Node* old_head = this->get_head();
+		this->set_head(node);
+		node->set_next(old_head);
+		node->set_prev(nullptr);
+	}
+	void pop_tail() {
+		Node* old_tail = this->get_tail();
+		this->set_tail(old_tail->get_prev());
+		delete old_tail;
+		this->get_tail()->set_next(nullptr);
+	}
+};
+
+enum class Direction {
+	Up,
+	Down,
+	Left,
+	Right
+};
+
 
 void render_square_from_coords(sf::RenderWindow &window, Coords coords, sf::Color color);
 
 int main()
 {
+	Direction direction = Direction::Right;
 	Coords apple_cords = Coords(7, 5);
 	sf::RenderWindow window( sf::VideoMode( { 800, 800 } ), "Snake" );
 	
@@ -84,7 +106,28 @@ int main()
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
+			{
 				window.close();
+			}
+			// managing keyboard inputs
+			if (const auto* key_pressed = event->getIf<sf::Event::KeyPressed>()) {
+				if (key_pressed->code == sf::Keyboard::Key::W)
+				{
+					direction = Direction::Up;
+				}
+				if (key_pressed->code == sf::Keyboard::Key::S)
+				{
+					direction = Direction::Down;
+				}
+				if (key_pressed->code == sf::Keyboard::Key::D)
+				{
+					direction = Direction::Right;
+				}
+				if (key_pressed->code == sf::Keyboard::Key::A)
+				{
+					direction = Direction::Left;
+				}
+			}
 		}
 
 		window.clear();
